ae876b6fb72c3761c330fc396a3e62519bd8c165
Audrey Robinel <arobinel@biolo.tech>
2020-06-18 22:43:43 -0400

    add support for bme680 and fix how result are printed

--- sensor-fetcher.py
+++ sensor-fetcher.py
@@ -17,10 +17,10 @@ import yaml
 import datetime as dt
 import sys
 from w1thermsensor import W1ThermSensor
-import board
-import busio
-import adafruit_tsl2561
-
+import board  # required for adafruit libs
+import busio  # required for adafruit libs
+import adafruit_tsl2561  # adafruit lib for tsl2561 lux sensor
+import adafruit_bme680  # adafruit lib for bme680 temp, pressure, humidity, gas
 
 if(len(sys.argv) != 2):
     print("ERROR: path of a config required file as first argument.")
@@ -40,6 +40,10 @@ DB_TABLE = config['DATABASE']['DB_TABLE']
 SENSORS_POLL_DELAY = config['general']['SENSORS_POLL_DELAY']
 SENSORS_POLL_AMOUNT = config['general']['SENSORS_POLL_AMOUNT']
 
+# Create library object using our Bus I2C port
+i2c = busio.I2C(board.SCL, board.SDA)
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+
 
 def ds18b20_temperature(probeid):
     """
@@ -57,7 +61,7 @@ def bme280_temperature(probeid):
     Get temperature from the first BME280 probe, in celcius.
 
     This method will return the temperature of the first BME280 probe found,
-    later on adresses to select will be added, as for now it is 0x27.
+    later on adresses to select will be added, as for now it is 0x76.
     """
     output = subprocess.check_output("/home/pi/.local/bin/read_bme280\
      --temperature | cut -f3 -d' '", shell=True).decode('utf-8')
@@ -69,7 +73,7 @@ def bme280_humidity(probeid):
     Get relative  humidity from the first bme280, in %RH.
 
     This method will return the relative humidity of the first BME280 probe
-    found, later on adresses to select will be added, as for now it is 0x27.
+    found, later on adresses to select will be added, as for now it is 0x76.
     """
     output = subprocess.check_output("/home/pi/.local/bin/read_bme280\
      --humidity | cut -f3 -d' '", shell=True).decode('utf-8')
@@ -81,11 +85,51 @@ def bme280_pressure(probeid):
     Get the atmospheric pressure from the first bme280, in hPa.
 
     This method will return the atmospheric pressure of the first BME280 probe
-    found, later on adresses to select will be added, as for now it is 0x27.
+    found, later on adresses to select will be added, as for now it is 0x76.
     """
     output = subprocess.check_output("/home/pi/.local/bin/read_bme280\
      --pressure | cut -f1 -d' '", shell=True).decode('utf-8')
     return float(output)
+
+
+def bme680_temperature(probeid):
+    """
+    Get temperature from the first bme680 probe, in celcius.
+
+    This method will return the temperature of the first BME680 probe found,
+    later on adresses to select will be added, as for now it is 0x77.
+    """
+    return float(bme680.temperature)
+
+
+def bme680_humidity(probeid):
+    """
+    Get relative  humidity from the first bme680, in %RH.
+
+    This method will return the relative humidity of the first bme680 probe
+    found, later on adresses to select will be added, as for now it is 0x77.
+    """
+    return float(bme680.humidity)
+
+
+def bme680_pressure(probeid):
+    """
+    Get the atmospheric pressure from the first bme680, in hPa.
+
+    This method will return the atmospheric pressure of the first bme680 probe
+    found, later on adresses to select will be added, as for now it is 0x77.
+    """
+    return float(bme680.pressure)
+
+
+def bme680_gas(probeid):
+    """
+    Get the gas levels from the first bme680, in ohm.
+
+    This method will return the gas levels (VOC, etc) of the first bme680 probe
+    found, later on adresses to select will be added, as for now it is 0x77.
+    """
+    return float(bme680.gas)
 
 
 def tsl2561_illuminance(probeid, gain=0, integ_time=1):
@@ -145,9 +189,11 @@ def insert_measure(value, sensor_id):
         # Commit your changes in the database
         db.commit()
         print("insert successful")
+        print("---")
     except Exception:
         # Rollback in case there is any error
         print("insert failed")
+        print("---")
         db.rollback()
     db.close()
 
@@ -160,24 +206,44 @@ def insert_node_measures():
     then insert the values in the database according to config.
     """
     for sensor in config['sensors']:
+        if(sensor['sensor_type'] == "ds18b20"):
+            value = ds18b20_temperature(sensor['probe_id'])
+            insert_measure(value, sensor['sensor_id'])
+
+        elif(sensor['sensor_type'] == "bme280temperature"):
+            value = bme280_temperature(sensor['probe_id'])
+            insert_measure(value, sensor['sensor_id'])
+
+        elif(sensor['sensor_type'] == "bme280humidity"):
+            value = bme280_humidity(sensor['probe_id'])
+            insert_measure(value, sensor['sensor_id'])
+
+        elif(sensor['sensor_type'] == "bme280pressure"):
+            value = bme280_pressure(sensor['probe_id'])
+            insert_measure(value, sensor['sensor_id'])
+
+        elif(sensor['sensor_type'] == "bme680temperature"):
+            value = bme680_temperature(sensor['probe_id'])
+            insert_measure(value, sensor['sensor_id'])
+
+        elif(sensor['sensor_type'] == "bme680humidity"):
+            value = bme680_humidity(sensor['probe_id'])
+            insert_measure(value, sensor['sensor_id'])
+
+        elif(sensor['sensor_type'] == "bme680pressure"):
+            value = bme680_pressure(sensor['probe_id'])
+            insert_measure(value, sensor['sensor_id'])
+
+        elif(sensor['sensor_type'] == "bme680gas"):
+            value = bme680_gas(sensor['probe_id'])
+            insert_measure(value, sensor['sensor_id'])
+
+        elif(sensor['sensor_type'] == "tsl2561"):
+            value = tsl2561_illuminance(sensor['probe_id'])
         print("sensor name : ", sensor['name'])
         print("sensor type : ", sensor['sensor_type'])
         print("sensor id : ", sensor['sensor_id'])
         print("probe id : ", sensor['probe_id'])
-        if(sensor['sensor_type'] == "ds18b20"):
-            value = ds18b20_temperature(sensor['probe_id'])
-            insert_measure(value, sensor['sensor_id'])
-        elif(sensor['sensor_type'] == "bme280temperature"):
-            value = bme280_temperature(sensor['probe_id'])
-            insert_measure(value, sensor['sensor_id'])
-        elif(sensor['sensor_type'] == "bme280humidity"):
-            value = bme280_humidity(sensor['probe_id'])
-            insert_measure(value, sensor['sensor_id'])
-        elif(sensor['sensor_type'] == "bme280pressure"):
-            value = bme280_pressure(sensor['probe_id'])
-            insert_measure(value, sensor['sensor_id'])
-        elif(sensor['sensor_type'] == "tsl2561"):
-            value = tsl2561_illuminance(sensor['probe_id'])
         print("value : ", value)
 
 
